268ab9dc0a0a10b773375e9c70cef432d24e7b9f
Himanshu Mahara <himmahar@publicisgroupe.net>
2022-06-29 20:01:33 +0530

    configure env

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import cartRouter from "./routes/cartRoutes";
+import * as dotenv from "dotenv";
+dotenv.config();
 //import { routes } from "./routes";
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node_ecomm");
+mongoose.connect(process.env.MONGO_CONNECT);
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ type: 'application/json' })); 
+app.use(bodyParser.json({ type: "application/json" }));
 
-
-
-
-
-app.use("/cart",cartRouter);
-app.listen(3002);
+app.use("/cart", cartRouter);
+app.listen(process.env.PORT,()=>{
+    console.log(`Cart is connected to ${process.env.PORT}`);
+});
